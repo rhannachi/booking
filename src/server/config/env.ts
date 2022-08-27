e7a8789d39d746f89614c3c9f131553c95c3ea36
Ramzi <ramzi.hannachi1991@gmail.com>
2022-08-27 12:32:10 +0200

    refactor: improvement var env

--- src/server/config/env.ts
+++ src/server/config/env.ts
@@ -1,16 +1,54 @@
-import 'dotenv/config'
+type NodeEnvType = 'development' | 'production' | 'test'
 
-type NameToType = {
-  readonly ENV: 'production' | 'staging' | 'development' | 'test'
-  readonly DB_LOCAL_URI: string
+interface EnvType {
+  dbUri: string
 }
 
-export function getEnv(name: keyof NameToType): NameToType[keyof NameToType] {
-  const val = process.env[name]
-
-  if (!val) {
-    throw new Error(`Cannot find environmental variable: ${name}`)
+export const getEnv = (): EnvType => {
+  let env: EnvType = {
+    dbUri: ''
   }
 
-  return val
+  try {
+    const nodeEnv = process.env.NODE_ENV as NodeEnvType
+
+    if (nodeEnv !== 'development' && nodeEnv !== 'production' && nodeEnv !== 'test') {
+      throw new Error('Env Error: NODE_ENV undefined')
+    }
+
+    let dbUri = ''
+
+    if (nodeEnv === 'development') {
+      dbUri = process.env?.DEV_DB_URI ?? ''
+
+      if (!dbUri) {
+        throw new Error('Env Error: DEV_DB_URI undefined')
+      }
+    }
+
+    if (nodeEnv === 'test') {
+      dbUri = process.env?.TEST_DB_URI ?? ''
+
+      if (!dbUri) {
+        throw new Error('Env Error: TEST_DB_URI undefined')
+      }
+    }
+
+    if (nodeEnv === 'production') {
+      dbUri = process.env?.PROD_DB_URI ?? ''
+
+      if (!dbUri) {
+        throw new Error('Env Error: PROD_DB_URI undefined')
+      }
+    }
+
+    env = {
+      dbUri
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+  } finally {
+    return env
+  }
 }
