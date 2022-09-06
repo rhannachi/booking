6337bb27bfb7f4eb8162ec108344484fe8181dd3
Ramzi <ramzi.hannachi1991@gmail.com>
2022-09-06 22:27:31 +0200

    feat: add icon component

--- src/components/Atoms/Button/Button.stories.tsx
+++ src/components/Atoms/Button/Button.stories.tsx
@@ -1,6 +1,4 @@
 import { Meta, Story } from '@storybook/react'
-import { within } from '@storybook/testing-library'
-import { expect } from '@storybook/jest'
 import Button, { ButtonProps } from './Button'
 
 type ButtonStorieProps = ButtonProps & {
@@ -17,13 +15,3 @@ export default {
 } as Meta<ButtonStorieProps>
 
 export const Default: Story<ButtonStorieProps> = (args) => <Button className={args.className}> {args.title} </Button>
-
-Default.args = {
-  title: 'Button Title'
-}
-
-Default.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const button = canvas.getByText('Button Title')
-  expect(button).not.toBeNull()
-}
