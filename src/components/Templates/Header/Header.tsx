77bf8bafb1a51875fcd4db8904822eab943068b4
Ramzi <ramzi.hannachi1991@gmail.com>
2022-09-16 20:31:01 +0200

    Refacto: move Header and Footer to templates

--- src/components/Templates/Header/Header.tsx
+++ src/components/Templates/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = ({}) => {
   return (
     <header className="container px-16 py-4 bg-slate-50 drop-shadow ">
       <nav className="flex items-center flex-row  justify-between ">
-        <img src="/booking.png" className="w-40" />
+        <img src="booking.png" className="w-40" />
         <Button className="text-sm font-semibold text-white bg-pink-500">Login</Button>
       </nav>
     </header>
