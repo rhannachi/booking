d162320db9bdc83f4624188eafa8ef7429f4a0d6
Ramzi <ramzi.hannachi1991@gmail.com>
2022-09-25 19:58:44 +0200

    add test thunk and slice room

---
+++ src/mocks/http.ts
@@ -0,0 +1,22 @@
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+import { API_GET_ROOMS } from '@/helpers'
+import { roomsMocked } from '@/mocks/fixtures'
+import { IApiRoomsResponse } from '@/shared/schemas'
+
+const fetchRoomsMocked: IApiRoomsResponse = {
+  status: 200,
+  all: 8,
+  count: 8,
+  limit: 8,
+  rooms: roomsMocked
+}
+
+export const mockNetWorkResponse = () => {
+  const mock = new MockAdapter(axios)
+  mock.onGet(`${API_GET_ROOMS}`).reply(200, fetchRoomsMocked)
+  // mock.onGet(`/users/`).reply(200, getUserListResponse);
+  // mock.onPost(`/users/`).reply(200, getCreateUserResponse);
+  // mock.onPut(`/users/${userId}`).reply(200, getUserUpdateResponse);
+  // mock.onDelete(`/users/${userId}`).reply(200);
+}
---
+++ src/store/room/room.test.ts
@@ -0,0 +1,43 @@
+import { roomInitialState, roomSlice } from './room.slice'
+import { store } from '@/store'
+import { mockNetWorkResponse } from '@/mocks'
+import { fetchRooms, FetchRoomsType } from '@/store/room/room.thunk'
+import { roomsMocked } from '@/mocks/fixtures'
+import { ROOM_SLICE_NAME } from '@/helpers'
+import { PayloadAction } from '@reduxjs/toolkit'
+
+/**
+ * Testing the initial state
+ */
+
+test('Should return initial state', () => {
+  expect(
+    roomSlice.reducer(undefined, {
+      type: undefined
+    })
+  ).toEqual(roomInitialState)
+})
+/**
+ * Testing the fetchUsers thunk
+ */
+
+describe('List all rooms', () => {
+  beforeAll(() => {
+    mockNetWorkResponse()
+  })
+
+  it('Should be able to fetch the room list', async () => {
+    // TODO remove type casting
+    const {
+      payload: { rooms },
+      type
+    } = (await store.dispatch(fetchRooms({ origin: '' }))) as PayloadAction<FetchRoomsType>
+
+    expect(type).toBe(`${ROOM_SLICE_NAME}/fetchRooms/fulfilled`)
+    expect(rooms).toEqual(roomsMocked)
+
+    const state = store.getState().roomReducer
+
+    expect(state.rooms).toEqual(roomsMocked)
+  })
+})
