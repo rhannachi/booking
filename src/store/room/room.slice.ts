90af9da4e3724a0c4afc274a336c7fe07f79dc3d
Ramzi <ramzi.hannachi1991@gmail.com>
2022-09-29 18:08:43 +0200

    fix: remove unused exports

--- src/store/room/room.slice.ts
+++ src/store/room/room.slice.ts
@@ -3,7 +3,48 @@ import { IRoom } from '@/shared/schemas'
 import { fetchRooms, FetchRoomsErrorType, FetchRoomsType } from '@/store/room/room.thunk'
 import { ROOM_SLICE_NAME } from '@/helpers/constants'
 
-export type RoomsStateType = Readonly<{
+/**
+ *  SET ROOMS ACTION
+ */
+type SetRoomsActionPayloadType = Readonly<{
+  rooms: IRoom[]
+}>
+
+const setRooms = (state: Draft<RoomsStateType>, action: PayloadAction<SetRoomsActionPayloadType>) => {
+  state.rooms = action.payload.rooms
+  state.isLoading = false
+}
+
+/***
+ *  FETCH ROOMS
+ */
+// PENDING
+const fetchRoomsPending = (state: Draft<RoomsStateType>) => {
+  state.rooms = []
+  state.isLoading = false
+}
+
+// SUCCESS
+const fetchRoomsFulfilled = (state: Draft<RoomsStateType>, action: PayloadAction<FetchRoomsType>) => {
+  const newAction: Readonly<PayloadAction<SetRoomsActionPayloadType>> = {
+    ...action,
+    payload: {
+      rooms: action.payload.rooms
+    }
+  }
+  roomSlice.caseReducers.setRoomsAction(state, newAction)
+}
+// REJECTED
+const fetchRoomsRejected = (state: Draft<RoomsStateType>, action: PayloadAction<FetchRoomsErrorType | undefined>) => {
+  state.rooms = []
+  state.error = action?.payload?.error
+  state.isLoading = false
+}
+
+/***
+ * SLICE ROOM
+ */
+type RoomsStateType = Readonly<{
   isLoading: boolean
   error?: string
   rooms: IRoom[]
@@ -14,53 +55,6 @@ export const roomInitialState: RoomsStateType = {
   error: '',
   isLoading: false
 }
-
-/**
- *  SET ROOMS ACTION
- */
-type SetRoomsActionPayloadType = Readonly<{
-  rooms: IRoom[]
-}>
-
-export const setRooms = (state: Draft<RoomsStateType>, action: PayloadAction<SetRoomsActionPayloadType>) => {
-  state.rooms = action.payload.rooms
-  state.isLoading = false
-}
-
-/***
- *  FETCH ROOMS
- */
-// PENDING
-export const fetchRoomsPending = (state: Draft<RoomsStateType>) => {
-  state.rooms = []
-  state.isLoading = false
-}
-
-// SUCCESS
-export const fetchRoomsFulfilled = (state: Draft<RoomsStateType>, action: PayloadAction<FetchRoomsType>) => {
-  const newAction: Readonly<PayloadAction<SetRoomsActionPayloadType>> = {
-    ...action,
-    payload: {
-      rooms: action.payload.rooms
-    }
-  }
-
-  roomSlice.caseReducers.setRoomsAction(state, newAction)
-}
-// REJECTED
-export const fetchRoomsRejected = (
-  state: Draft<RoomsStateType>,
-  action: PayloadAction<FetchRoomsErrorType | undefined>
-) => {
-  state.rooms = []
-  state.error = action?.payload?.error
-  state.isLoading = false
-}
-
-/***
- *
- */
-
 export const roomSlice = createSlice({
   name: ROOM_SLICE_NAME,
   initialState: roomInitialState,
