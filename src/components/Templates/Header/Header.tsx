bb41d6d6429c67860cef7fec4187c899b7bdff7d
Ramzi <ramzi.hannachi1991@gmail.com>
2022-09-29 21:37:55 +0200

    feat: connect home container with view

--- src/components/Templates/Header/Header.tsx
+++ src/components/Templates/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const Header = ({}) => {
   return (
-    <header className="container px-16 py-4 bg-slate-50 drop-shadow ">
+    <header className=" px-16 py-4 bg-slate-50 drop-shadow fixed  top-0 left-0 w-full z-10">
       <nav className="flex items-center flex-row  justify-between ">
         <img src="booking.png" alt="booking icon" className="w-40" />
         <Button className="text-sm font-semibold text-white bg-pink-500">Login</Button>
