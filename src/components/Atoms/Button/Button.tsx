6f70da91d0d614cdd64d1710836b7f7e076020cd
Ramzi <ramzi.hannachi1991@gmail.com>
2022-10-06 18:32:23 +0200

    feat: add toast component
    TODO
    
    
    TODO
    
    
    TODO
    
    
    TODO
    
    
    TODO

--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -1,10 +1,16 @@
 import clsx from 'clsx'
-import React, { PropsWithChildren } from 'react'
+import React, { MouseEvent, PropsWithChildren, ReactNode } from 'react'
 
 export type ButtonProps = {
   className?: string
+  children: ReactNode
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void // add addon storybook
 }
 
-export const Button = ({ children, className = 'text-white bg-cyan-400' }: PropsWithChildren<ButtonProps>) => {
-  return <button className={clsx(className, 'py-2 px-6 font-semibold rounded-3xl ')}>{children}</button>
+export const Button = ({ children, className = 'text-white bg-cyan-400', onClick }: PropsWithChildren<ButtonProps>) => {
+  return (
+    <button onClick={onClick} className={clsx(className, 'py-2 px-6 font-semibold rounded-3xl ')}>
+      {children}
+    </button>
+  )
 }
